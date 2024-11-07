d560afab5128161f86bfe43a4ed0f9d17264f123
anthonysfonseca <anthonyfonseca00@gmail.com>
2024-11-06 23:50:41 -0800

    Lab 11 - 11.3) Exercise 1

--- array_struct.cpp
+++ array_struct.cpp
@@ -4,20 +4,26 @@ using namespace std;
 
 // This program demonstrates how to use an array of structures
 
-// PLACE YOUR NAME HERE
+// Anthony Fonseca
 
 // Fill in code to declare a structure called taxPayer that has three
-// members:	taxRate, income, and taxes — each of type float
+// members:	taxRate, income, and taxes - each of type float
+struct taxPayer {
+	float taxRate;
+	float income;
+	float taxes;
+};
 
 int main()
 {
 	// Fill in code to define an array named citizen which holds
 	// 5 taxPayers structures
+	taxPayer citizen[5];
 
 	cout << fixed << showpoint << setprecision(2);
 
-	cout << "Please enter the annual income and tax rate for 5 tax payers: ";
-	cout << endl << endl << endl;
+	cout << "Please enter the annual income and tax rate for 5 tax payers";
+	cout << endl << endl;
 
 	for (int count = 0; count < 5; count++)
 	{
@@ -25,14 +31,17 @@ int main()
 		cout << ": ";
 
 		// Fill in code to read in the income to the appropriate place
+		cin >> citizen[count].income;
 
 		cout << "Enter the tax rate for tax payer # " << (count + 1);
 		cout << ": ";
 
 		// Fill in code to read in the tax rate to the appropriate place
+		cin >> citizen[count].taxRate;
 
 		// Fill in code to compute the taxes for the citizen and store it
 		// in the appropriate place
+		citizen[count].taxes = citizen[count].income * citizen[count].taxRate;
 
 		cout << endl;
 	}
@@ -41,6 +50,7 @@ int main()
 
 	// Fill in code for the first line of a loop that will output the
 	// tax information
+	for (int index = 0; index < 5; index++)
 	{
 		cout << "Tax Payer # " << (index + 1) << ": " << "$ "
 		     << citizen[index].taxes << endl;
